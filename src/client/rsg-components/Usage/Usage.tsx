422294a5c56b9940f4a6069f93cf0b0639cf027a
elevatebart <ledouxb@me.com>
2020-03-12 17:34:18 -0500

    rmove props

--- src/client/rsg-components/Usage/Usage.tsx
+++ src/client/rsg-components/Usage/Usage.tsx
@@ -1,34 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { MethodDescriptor } from 'react-docgen';
-import { PropDescriptorWithFlow } from 'rsg-components/Props/util';
-import Props from 'rsg-components/Props';
-import Methods from 'rsg-components/Methods';
-import isEmpty from 'lodash/isEmpty';
+import React from "react";
+import PropTypes from "prop-types";
+import { MethodDescriptor } from "react-docgen";
+import { PropDescriptorWithFlow } from "rsg-components/Props/util";
+import Methods from "rsg-components/Methods";
+import isEmpty from "lodash/isEmpty";
 
 const Usage: React.FunctionComponent<{
-	props: { methods?: MethodDescriptor[]; props?: PropDescriptorWithFlow[] };
-}> = ({ props: { props, methods } }) => {
-	const propsNode = props && !isEmpty(props) && <Props props={props} />;
-	const methodsNode = methods && !isEmpty(methods) && <Methods methods={methods} />;
+  props: { methods?: MethodDescriptor[] };
+}> = ({ props: { methods } }) => {
+  const methodsNode = methods && !isEmpty(methods) && (
+    <Methods methods={methods} />
+  );
 
-	if (!propsNode && !methodsNode) {
-		return null;
-	}
+  if (!methodsNode) {
+    return null;
+  }
 
-	return (
-		<div>
-			{propsNode}
-			{methodsNode}
-		</div>
-	);
+  return <div>{methodsNode}</div>;
 };
 
 Usage.propTypes = {
-	props: PropTypes.shape({
-		props: PropTypes.array,
-		methods: PropTypes.array,
-	}).isRequired,
+  props: PropTypes.shape({
+    methods: PropTypes.array
+  }).isRequired
 };
 
 export default Usage;
