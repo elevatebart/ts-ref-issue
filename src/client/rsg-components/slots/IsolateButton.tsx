e25915a93d14b788d98b6dd524e508b83337856c
elevatebart <ledouxb@me.com>
2020-03-12 17:12:38 -0500

    remove dependencies

--- src/client/rsg-components/slots/IsolateButton.tsx
+++ src/client/rsg-components/slots/IsolateButton.tsx
@@ -1,41 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { MdFullscreen, MdFullscreenExit } from 'react-icons/md';
-import ToolbarButton from 'rsg-components/ToolbarButton';
-import getUrl from '../../utils/getUrl';
+import React from "react";
+import PropTypes from "prop-types";
+import { MdFullscreen, MdFullscreenExit } from "react-icons/md";
+import ToolbarButton from "rsg-components/ToolbarButton";
 
 export interface IsolateButtonProps {
-	name: string;
-	example?: number;
-	isolated?: boolean;
+  name: string;
+  example?: number;
+  isolated?: boolean;
 }
 
 const IsolateButton = ({ name, example, isolated }: IsolateButtonProps) => {
-	const testID = example ? `${name}-${example}-isolate-button` : `${name}-isolate-button`;
+  const testID = example
+    ? `${name}-${example}-isolate-button`
+    : `${name}-isolate-button`;
 
-	return isolated ? (
-		<ToolbarButton
-			href={getUrl({ anchor: true, slug: name.toLowerCase() })}
-			title="Show all components"
-			testId={testID}
-		>
-			<MdFullscreenExit />
-		</ToolbarButton>
-	) : (
-		<ToolbarButton
-			href={getUrl({ name, example, isolated: true })}
-			title="Open isolated"
-			testId={testID}
-		>
-			<MdFullscreen />
-		</ToolbarButton>
-	);
+  return isolated ? (
+    <ToolbarButton href="#" title="Show all components" testId={testID}>
+      <MdFullscreenExit />
+    </ToolbarButton>
+  ) : (
+    <ToolbarButton href="#" title="Open isolated" testId={testID}>
+      <MdFullscreen />
+    </ToolbarButton>
+  );
 };
 
 IsolateButton.propTypes = {
-	name: PropTypes.string.isRequired,
-	example: PropTypes.number,
-	isolated: PropTypes.bool,
+  name: PropTypes.string.isRequired,
+  example: PropTypes.number,
+  isolated: PropTypes.bool
 };
 
 export default IsolateButton;
